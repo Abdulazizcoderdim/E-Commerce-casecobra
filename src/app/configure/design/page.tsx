387df987c60f9fabe9abdecbf4bf868379e26f8f
AbdulazizRus <coderdim@gmail.com>
2024-05-31 07:15:22 +0500

    canges

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,11 +1,36 @@
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
+import DesignConfigurator from './DesignConfigurator'
 
-const Page = () => {
-// make db call 
-  
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams
+
+  if (!id || typeof id !== 'string') {
+    return notFound()
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  })
+
+  if (!configuration) {
+    return notFound()
+  }
+
+  const { imageUrl, height, width } = configuration
+
   return (
-    <p>
-        
-    </p>
+    <DesignConfigurator
+      imageUrl={imageUrl}
+      imageDimensions={{ width, height }}
+      configId={configuration.id}
+    />
   )
 }
 
