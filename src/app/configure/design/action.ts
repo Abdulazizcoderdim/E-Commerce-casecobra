1b1c659a07a8cbaa9c4a744b858c2e8fa293638a
AbdulazizRus <coderdim@gmail.com>
2024-06-05 16:01:56 +0500

    canges

--- src/app/configure/design/action.ts
+++ src/app/configure/design/action.ts
@@ -1,11 +1,23 @@
 'use server'
 
+import { db } from "@/db"
+import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from "@prisma/client"
+
 export async function saveConfig({
   color,
   finish,
   material,
   model,
   configId,
-}) {}
-
-//6:20:00
+}: {
+  color: CaseColor
+  finish: CaseFinish
+  material: CaseMaterial
+  model: PhoneModel
+  configId: string
+}) {
+  await db.configuration.update({
+    where: { id: configId },
+    data: {color, finish, material, model},
+  })
+}
