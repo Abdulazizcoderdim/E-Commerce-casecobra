9c07d8c8159ad47fdf5ed0a9064fa5a7aceb00fb
AbdulazizRus <coderdim@gmail.com>
2024-06-09 12:54:43 +0500

    ads

--- src/app/configure/preview/action.ts
+++ src/app/configure/preview/action.ts
@@ -3,6 +3,7 @@
 import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { Order } from '@prisma/client'
 
 export const createCheckoutSession = async ({
   configId,
@@ -25,4 +26,25 @@ export const createCheckoutSession = async ({
  let price = BASE_PRICE
  if(finish === "textured") price += PRODUCT_PRICES.finish.textured
  if(material === "polycarbonate") price += PRODUCT_PRICES.material.polycarbonate
+ 
+ let order: Order | undefined = undefined
+
+ const existingOrder = await db.order.findFirst({
+   where: {
+     userId: user?.id,
+     configurationId: configuration.id,
+   }
+ })
+
+ if(existingOrder) {
+  order = existingOrder
+ }else{
+  order = await db.order.create({
+    data: {
+      amount: price / 100,
+      userId: user?.id,
+      configurationId: configuration.id,
+    }
+  })
+ }
 }
